95e05701459fda4931014b3e1e3e18c3697e7373
Arthur Clement <aclement2@gmail.com>
2020-11-23 22:16:06 +1100

    heroku build wip

--- heroku-build.sh
+++ heroku-build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-echo "hello ${PORT}"
 env
+
+case $APP_NAME in
+    notello-gw) 
+        echo "Building $APP_NAME"
+    ;;
+    *) 
+        echo "Unknow app name $APP_NAME"
+        exit 1
+    ;;
+esac
+
+
